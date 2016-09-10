b955a213bdbd9b73fc630c05c6faf0b9d06f613c
John Urbank <jurbank@fallsdigital.com>
2016-09-10 08:50:53 -0400

    parallax demo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,23 @@
 'use strict';
 
-$('#contact-toggle').on('click', function(e){
-	e.preventDefault();
-	$(this).parent().toggleClass('active');
-});
+  var scenea = document.getElementById('scene');
+  var parallax = new Parallax(scenea);  
+
+
+for (var i = 1; i <= 25; i++) {
+  var scene = document.getElementById('scene-' + i);
+  var parallax = new Parallax(scene, {
+    calibrateX: false,
+    calibrateY: true,
+    scalarX: -80,
+    scalarY: 20,
+    frictionX: 0.8,
+    frictionY: 0.8,
+    originX: 0.0,
+    originY: 1.0
+  });
+}
+
+
+
+
