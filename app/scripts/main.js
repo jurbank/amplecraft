dceb16e895137da930ccc9be3950b939bb72b017
John Urbank <jurbank@fallsdigital.com>
2016-10-14 23:10:58 -0400

    updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,84 @@
 'use strict';
 
-$('#contact-toggle').on('click', function(e){
-	e.preventDefault();
-	$(this).parent().toggleClass('active');
+
+
+
+// $('.slider').slick({
+//   slidesToShow: 2,
+//   arrows: false,
+//   responsive: [
+//      {
+//       breakpoint: 1400,
+//       settings: {
+//         arrows: false,
+//         slidesToShow: 2
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 1
+//       }
+//     }
+//   ]
+// });
+
+
+  var equalheight = function(container){
+
+  var currentTallest = 0,
+       currentRowStart = 0,
+       rowDivs = new Array(),
+       $el,
+       topPosition = 0;
+   $(container).each(function() {
+
+     $el = $(this);
+     console.log($el)
+     $($el).height('auto')
+     var topPostion = $el.position().top;
+
+
+     if (currentRowStart != topPostion) {
+       for (var currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+         rowDivs[currentDiv].height(currentTallest);
+       }
+       rowDivs.length = 0; // empty the array
+       currentRowStart = topPostion;
+       currentTallest = $el.height();
+       rowDivs.push($el);
+     } else {
+       rowDivs.push($el);
+       currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+    }
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+   });
+  }  
+
+
+
+
+$(window).load(function() {
+  equalheight('.equalize');
+  equalheight('.equalize-2');
 });
+
+
+$(window).resize(function(){
+  equalheight('.equalize');
+  equalheight('.equalize-2');
+});
+
+
+
+
+
+
+
+
+
